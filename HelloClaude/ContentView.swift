60b8546b3db5076dcb3d2e8e1e83e63a39acff92
Masato999 <69575931+Masato999@users.noreply.github.com>
2026-08-12 16:47:26 +0900

    Initial Commit

---
+++ HelloClaude/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloClaude
+//
+//  Created by 高橋正人 on 2026/08/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloClaude/HelloClaudeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloClaudeApp.swift
+//  HelloClaude
+//
+//  Created by 高橋正人 on 2026/08/12.
+//
+
+import SwiftUI
+
+@main
+struct HelloClaudeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
